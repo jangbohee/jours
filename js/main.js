b215ba163534f770d14caeb638025067b150bc46
jangbohee <rakamin2830@gmail.com>
2018-08-06 14:53:09 +0900

    메인 수정

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#header").load("https://jangbohee.github.io/jours/nav.html");
-		var swiper = new Swiper('.swiper-container', {
+		var swiper1 = new Swiper('.swiper-container1', {
 		 loop: true,
 		 autoplay: {
 			delay: 2500,
@@ -10,5 +10,18 @@ $(function(){
 		 pagination: {
 			el: '.swiper-pagination',
 		  },
-    });
+	});
+	var swiper2 = new Swiper('.swiper-container2', {
+		slidesPerView: 6,
+		spaceBetween: 30,
+		loop: true,
+		autoplay: {
+		   delay: 800,
+		   disableOnInteraction: false,
+		},
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	  });
 })
